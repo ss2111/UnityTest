943b296cfd2145f3cf05da43840dd037268e8404
Scott Stephan <ss2111@msstate.edu>
2015-03-23 21:30:36 -0500

    Spawning Limit
    Stops the blocks from spawning when reaching a certain 'y' value

--- UnityTest/Assets/Scott Test/SpawnOnSprite.cs
+++ UnityTest/Assets/Scott Test/SpawnOnSprite.cs
@@ -1,14 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class SpawnOnSprite : MonoBehaviour {
 
 	public Vector3 ObjectSpawnPosition;
 	public GameObject obj;
 	public Vector3 ObjectSpawnPosition2;
 	public GameObject obj2;
-	public float BoxYPosition = -100;
 
+
+	public bool checkIfPosEmpty(Vector3 targetPos){
+		GameObject[] allGems = GameObject.FindGameObjectsWithTag("Gem");
+		GameObject lastItem = allGems [allGems.Length - 1];
+		if(lastItem.transform.position.y >= targetPos.y){
+			if(Mathf.Round(lastItem.transform.position.x) != targetPos.x){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		return true;
+	}
 	// Update is called once per frame
 	void Update () {
 		float x = Random.Range(0,2);
@@ -18,10 +32,12 @@ public class SpawnOnSprite : MonoBehaviour {
 			}
 
 			if(x==1){
+				if(checkIfPosEmpty(ObjectSpawnPosition2)){
 					GameObject.Instantiate(obj2, ObjectSpawnPosition2, Quaternion.identity);
-					BoxYPosition = obj2.transform.position.y;
 
+				}
 			}
 		}
 	}
+
 }
